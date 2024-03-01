bdbaa64d8b5bbf372604fc729726854ef230db4a
mudassarali.official <mudassarali.official@gmail.com>
2024-03-01 06:11:22 +0500

    Components Example In Angular

---
+++ src/app/Components/components-example/components-example.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components-example',
+  templateUrl: './components-example.component.html',
+  styleUrl: './components-example.component.css'
+})
+export class ComponentsExampleComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsExampleComponent } from './Components/components-example/components-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsExampleComponent
   ],
   imports: [
     BrowserModule,
